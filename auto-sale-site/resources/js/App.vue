477bf0e97fda33b521046e1471cba37c1e343d7c
KlonedBorn <michealdaivd132@gmail.com>
2024-04-28 09:28:07 -0400

    + Removed blade.php files. + Completed setting up Vue

---
+++ auto-sale-site/resources/js/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "./Components/Header.vue";
+import Footer from "./Components/Footer.vue";
+</script>
+
+<template>
+    <Header />
+    <div class="container my-5">
+        <RouterView />
+    </div>
+    <Footer />
+</template>
+
+<style scoped>
+
+</style>
--- auto-sale-site/routes/web.php
+++ auto-sale-site/routes/web.php
@@ -1,13 +1,4 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/login', function () { return view('login'); });
+Route::get('/{any?}', function () {
+    return view('welcome');
+})->where('any', '.*');
